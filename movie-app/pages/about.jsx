c7220b56a215718f3b19949d71d44d17d648dc3f
Alex M <57605252+MasterSancho@users.noreply.github.com>
2020-09-26 21:30:57 +0300

    003-Part 7
    7-Reusable Components

--- movie-app/pages/about.jsx
+++ movie-app/pages/about.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 // functional component - arrow function
 // const About = () => {
 //     const message = 'Hello World';
@@ -18,9 +16,9 @@ import React from 'react';
 // }
 
 class About extends React.Component {
-    render() {
-        return <h1>Hello I am class component</h1>;
-    }
+  render() {
+    return <h1>Hello I am class component</h1>;
+  }
 }
 
 export default About;
