93de54dd1c434b49316737e55def2efd34902651
Alex M <57605252+MasterSancho@users.noreply.github.com>
2020-09-26 13:41:10 +0300

    001-Part 3-4-5
    3-Application Initialization
    4-Create index page
    5-Functional Components

---
+++ movie-app/pages/about.js
@@ -0,0 +1,13 @@
+// functional component - arrow function
+const About = () => {
+    const message = 'Hello World';
+
+    return <h1>Hello About Page - {message}</h1>;
+};
+
+// functional component - normal function
+// function About() {
+//     return <h1>Hello About</h1>;
+// }
+
+export default About;
