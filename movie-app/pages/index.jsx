e69afebe9a364f44fb91f960c641dc88a3ef90d8
Alex M <57605252+MasterSancho@users.noreply.github.com>
2020-09-27 10:45:54 +0300

    004-Part 8
    8-Some basics styles

--- movie-app/pages/index.jsx
+++ movie-app/pages/index.jsx
@@ -14,42 +14,49 @@ export default function Home() {
           rel='stylesheet'
           href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
           integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
-          crossorigin='anonymous'
+          crossOrigin='anonymous'
         />
         <script
           src='https://code.jquery.com/jquery-3.3.1.slim.min.js'
           integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo'
-          crossorigin='anonymous'
+          crossOrigin='anonymous'
         ></script>
         <script
           src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'
           integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1'
-          crossorigin='anonymous'
+          crossOrigin='anonymous'
         ></script>
         <script
           src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
           integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM'
-          crossorigin='anonymous'
+          crossOrigin='anonymous'
         ></script>
       </Head>
 
       <Navbar />
-
-      <div className='container'>
-        <div className='row'>
-          <div className='col-lg-3'>
-            <SideMenu />
-          </div>
-
-          <div className='col-lg-9'>
-            <Carousel />
-            <div className='row'>
-              <MovieList />
+      <div className='home-page'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-3'>
+              <SideMenu />
+            </div>
+            <div className='col-lg-9'>
+              <Carousel />
+              <div className='row'>
+                <MovieList />
+              </div>
             </div>
           </div>
         </div>
       </div>
       <Footer />
+      <style jsx>
+        {`
+          .home-page {
+            padding-top: 80px;
+          }
+        `}
+      </style>
     </div>
   );
 }
