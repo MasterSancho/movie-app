b0150ace8e38603d71993d3f006b8ceae945982b
Alex M <57605252+MasterSancho@users.noreply.github.com>
2020-09-28 15:14:53 +0300

    011-Part 16
    16-Shorten Function

--- movie-app/components/MovieList.jsx
+++ movie-app/components/MovieList.jsx
@@ -1,6 +1,14 @@
 class MovieList extends React.Component {
+  shorten = (text, maxLength) => {
+    if (text && text.length > maxLength) {
+      return text.substr(0, maxLength) + '...';
+    }
+    return text;
+  };
+
   renderMovies() {
     const { movies } = this.props;
+
     return movies.map(movie => (
       <div className='col-lg-4 col-md-6 mb-4'>
         <div className='card h-100'>
@@ -11,7 +19,7 @@ class MovieList extends React.Component {
             <h4 className='card-title'>
               <a href='#'>{movie.name}</a>
             </h4>
-            <p className='card-text'>{movie.description}</p>
+            <p className='card-text'>{this.shorten(movie.description, 100)}</p>
           </div>
           <div className='card-footer'>
             <small className='text-muted'>&#9733; {movie.rating}</small>
